b51cea69bdef72fa5b1d2e5fd22cd4825962d586
Juhasz Zsolt <juhasz.zsolt98@gmail.com>
2021-10-09 11:46:14 +0200

    Vehicle.java kesz

--- src/models/Vehicle.java
+++ src/models/Vehicle.java
@@ -16,19 +16,8 @@
 
 package models;
 
-/**
- * A járművek bármilyen kerekes és kerék nélküli eszközök lehetnek.
- * Lehet akár egy Lada, egy szekér. De monjuk az utóbbi típusának
- * a feliírása nem könnyű ebben az oszhtályban. Rendszáma
- * meg pláne nincs a szekereknek, bicikliknek. De mivel
- * személyautókat eladó cégnek készül, jó lesz ez így.
- */
-
 public class Vehicle {
-    // ordinal tároló:
     public String ordinal;
-    // brand tároló:
     public String brand;
-    // year tároló:
     public String year;    
-}// A Vehicle vége
+}
